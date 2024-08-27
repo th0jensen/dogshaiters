3330cb8ec538343b9bc74ddf2a6dd3b570b01180
Thomas Jensen <thomas.jensen_@outlook.com>
2024-08-27 13:04:58 +0100

    Updated repo

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@ use bevy::prelude::*;
 use dogshaiters::GamePlugin;
 use wasm_bindgen::prelude::*;
 
+#[bevy_main]
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                canvas: Some("#canvas".into()),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(GamePlugin)
         .run();
 }
